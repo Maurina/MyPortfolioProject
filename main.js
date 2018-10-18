c99ecd7dbbfeb95f28a66e02ac55806ef5215021
Maurina <Maurina.Davis@gmail.com>
2018-10-18 09:13:55 -0600

    javascript added

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import { films } from './assets/films.js'
 import { people } from './assets/people.js'
 import { starships } from './assets/starships.js'
 
+console.log(films)
+
 let filmList = document.querySelector("#film-list")
 
 films.forEach((film) => {
@@ -11,8 +13,18 @@ films.forEach((film) => {
     console.log(film.title)
 })
 
-console.log(people[0])
+let filmShip = document.querySelector("#film-people")
+
+starships.forEach((starship) => {
+    let listItem = document.createElement('li')
+    listItem.textContent = starship.name
+    filmShip.appendChild(listItem)
+    console.log(starship.name)
+})
 
 
-
-
+let openingCrawl = document.querySelector('#film-crawl')
+let listItem = document.createElement('li')
+listItem.textContent = films[0].opening_crawl
+openingCrawl.appendChild(listItem)
+console.log(films.opening_crawl)
