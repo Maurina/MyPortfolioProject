a2eefd6fd408b175e013918045aaebbb86bfce78
Maurina <Maurina.Davis@gmail.com>
2018-11-10 09:46:31 -0700

    cardBack added to javascript

--- pokecode.js
+++ pokecode.js
@@ -2,85 +2,39 @@ import { pokemon } from './pokemon.js'
 
 console.log(pokemon)
 
-
-
-/*const pokeContainer = document.querySelector('#container')
-
-
+const pokeContainer = document.querySelector('#container')
 pokemon.forEach(poke => {
-
-const card = document.createElement('div')
-
+const card = document.createElement('figure')
  console.log(`${poke.id}${poke.ename}.png`)
  let fig = document.createElement('figure')
  let cap = document.createElement('figcaption')
  let img = document.createElement('img')
+
  img.src = `images/${poke.id}${poke.ename}.png`
  cap.textContent = poke.ename
  fig.appendChild(cap)
  fig.appendChild(img)
  card.appendChild(fig)
-
  pokeContainer.appendChild(card)
- card.addEventListener( 'click', function(){
-     card.classList.toggle('is-flipped');
- })    
-})
+ 
+  })    
 
-const cardBack = document.createElement('div')
+const pokeBackContainer = document.querySelector("#backContainer")
+pokemon.forEach(poke => {
+const cardBack = document.createElement('figure')
 let fig = document.createElement('figure')
 let cap = document.createElement('figcaption')
 let img = document.createElement('img')
+
 img.src = `images/PokemonLogo`
 cap.textContent = poke.ename
 fig.appendChild(cap)
 fig.appendChild(img)
 cardBack.appendChild(fig)
-cardBack.appendChild(backDiv)
-cardBack.addEventListener('click', function() {
+pokeBackContainer.appendChild(cardBack)
+})  
+
+let card = document.querySelector('.card');
+card__face.addEventListener( 'click', function(){
     card.classList.toggle('is-flipped');
 })
-
-
-
-let backDiv = document.createElement('div')
-backDiv.textContent = poke.cname */
-
-const fontFig = (element) => {
-    let fig = document.createElement('figure')
-    fig.className =".card__face--front"
-    let cap = document.createElement('figcaption')
-    let img = document.createElement('img')
-    img.src = `images/${poke.id}${poke.ename}.png`
-    cap.textContent = poke.ename
-    fig.appendChild(cap)
-    fig.appendChild(img)
-
-}
-
-const backFig = (element) =>{
- let fig = doucoment.createElement('figure')
- fig.className ="card__face--back"
- let img = document.createElement('img')
- img.src = `images/PokemonLogo`
- fig.appendChild(cap)
- fig.appendChild(img)
-
- 
-}
-
-
-pokemon.forEach(poke => {
-    let card = document.createElement('div')
-    card.className ="card "
-    card.appendChild(frontFig(poke))
-    card.appendChild(backFig(poke))
-    pokeContainer.appendChild(card)
-    card.addEventListener('click', function() {
-        console.log(poke.ename)
-        card.classList.toggle('is-flipped');
-
-    });
-})
-
-//animated bounce infinite delay-2s
