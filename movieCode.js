4da79c8f9b28216019455352ffba400143a10a7b
Maurina Davis <Maurina.Davis@gmail.com>
2018-11-28 13:24:36 -0700

    javascript DOM information added

--- movieCode.js
+++ movieCode.js
@@ -2,7 +2,14 @@
 import { movies } from './movieInformation.js'
 
 
+// Constructor Function
 
+function MovieInfo(original_title, overview, popularity) {
+    this.original_title = original_title
+    this.overview = overview
+    this.popularity = popularity
+  
+}
 
 
 
@@ -11,22 +18,31 @@ movies.forEach(movie => {
 /*---------------------- Poster Image and Tagline -------------------------------------------------- */
 let moviePoster = document.querySelector('.poster')
     let moviePic = document.createElement('img')
-    let movieFig = document.createElement('figure')
-    let movieCap = document.createElement('figcaption')
+   
     moviePic.src = `images/${movie.id}.jpg`
 
-    movieCap.textContent = movie.tagline
-    movieFig.appendChild(moviePic)
-    movieFig.appendChild(movieCap)
-    moviePoster.appendChild(movieFig)
+    moviePoster.appendChild(moviePic)
 
 /*-------------------------------------- Movie Info -------------------------------------------------- */
     let movieText = document.querySelector('.info')
+    
+    let title = document.createElement('h2')
+    title.textContent = movie.original_title
+
+    let tagline = document.createElement('p')
+    tagline.textContent = movie.tagline
+    
+    let popular = document.createElement('p')
+    popular.textContent =`Popularity rating : ${movie.popularity}`
+    
     let quote = document.createElement('blockquote')
     quote.textContent = movie.overview
+    
+
+    movieText.appendChild(title)
+    movieText.appendChild(tagline)
+    movieText.appendChild(popular)
     movieText.appendChild(quote)
-
-
 })
 
 
